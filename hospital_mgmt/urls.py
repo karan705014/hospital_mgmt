a693721facc3ae149fe3433c24519882cd740084
ecommerce <aditya705014@gmail.com>
2025-10-03 11:04:29 +0530

    nothing

--- hospital_mgmt/urls.py
+++ hospital_mgmt/urls.py
@@ -19,6 +19,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-        path("", include("hospital_app.urls")),
+    path("", include("hospital_app.urls")),
 
 ]
